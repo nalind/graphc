053d6a64a3c35dda0755c7dbeb40f2e29320ce33
Nalin Dahyabhai <nalin@redhat.com>
2015-09-21 15:19:27 -0400

    Wrap graph.GetParent()

---
+++ parent.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func imageParent(c *cli.Context) {
+	graph := initGraph(c)
+	id := c.Args().First()
+	image, err := graph.Get(id)
+	if err != nil {
+		fmt.Printf("Failed to read image %s: %v\n", id, err)
+		os.Exit(1)
+	}
+	parent, err := graph.Get(image.Parent)
+	if err != nil {
+		fmt.Printf("Failed to read image %s: %v\n", image.Parent, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", parent.ID)
+}
+
+func init() {
+	commands = append(commands, cli.Command{
+		Name:   "parent",
+		Usage:  "list an image's parent image",
+		Action: imageParent,
+	})
+}
