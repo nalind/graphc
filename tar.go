3d4db3ce1087ac582c977214f1def4f7caa2ea5e
Nalin Dahyabhai <nalin@redhat.com>
2015-09-21 16:02:46 -0400

    Wrap graph.TarLayer()

---
+++ tar.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func tarImage(c *cli.Context) {
+	graph := initGraph(c)
+	id := c.Args().First()
+	image, err := graph.Get(id)
+	if image == nil || err != nil {
+		if err != nil {
+			fmt.Printf("Failed to find image layer %s: %v\n", id, err)
+		} else {
+			fmt.Printf("Failed to find image layer %s\n", id)
+		}
+		os.Exit(1)
+	}
+	tar, err := graph.TarLayer(image)
+	if tar == nil || err != nil {
+		if err != nil {
+			fmt.Printf("Failed to tar image layer %s: %v\n", id, err)
+		} else {
+			fmt.Printf("Failed to tar image layer %s\n", id)
+		}
+		os.Exit(1)
+	}
+	outfile := os.Stdout
+	if c.String("output") != "" {
+		outfile, err = os.Create(c.String("output"))
+		if err != nil {
+			fmt.Printf("Error opening %s for output: %s\n", c.String("output"), err)
+		}
+	}
+	if _, err := io.Copy(outfile, tar); err != nil {
+		if err != nil {
+			fmt.Printf("Error writing image to output: %s\n", err)
+		} else {
+			fmt.Printf("Error writing image to output\n")
+		}
+		os.Exit(1)
+	}
+}
+
+func init() {
+	commands = append(commands, cli.Command{
+		Name:  "tar",
+		Usage: "produce an image tarball",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "output, o",
+				Value: "",
+				Usage: "an output file",
+			},
+		},
+		Action: tarImage,
+	})
+}
