668b457a7c12c54a0ae5f013aabedf327b8a5c3b
Nalin Dahyabhai <nalin@redhat.com>
2015-09-28 17:14:12 -0400

    Start wrapping ExportImage()

---
+++ export.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func exportImage(c *cli.Context) {
+	ts, _, _ := initTagStore(c)
+	outfile := os.Stdout
+	if c.String("output") != "" {
+		newfile, err := os.Open(c.String("output"))
+		if err != nil {
+			fmt.Printf("Error opening output file: %s\n", err)
+			os.Exit(1)
+		}
+		outfile = newfile
+	}
+	err := ts.ImageExport(c.Args(), outfile)
+	if err != nil {
+		fmt.Printf("Error exporting: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	commands = append(commands, cli.Command{
+		Name:  "export",
+		Usage: "export an image",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "output, o",
+				Value: "",
+				Usage: "an output file",
+			},
+		},
+		Action: exportImage,
+	})
+}
