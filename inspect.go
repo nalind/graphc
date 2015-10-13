2e0d44d665746f6af3575d41076b9c2f8a36ff04
Nalin Dahyabhai <nalin@redhat.com>
2015-10-13 15:54:55 -0400

    Add a more conventional inspect command

---
+++ inspect.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func inspectImage(c *cli.Context) {
+	ts, _, _ := initTagStore(c)
+	id := c.Args().First()
+	idata, err := ts.Lookup(id)
+	if err != nil {
+		fmt.Printf("Failed to locate image %s: %s\n", id, err)
+		os.Exit(1)
+	}
+	outfile := os.Stdout
+	if c.String("output") != "" {
+		outfile, err = os.Create(c.String("output"))
+		if err != nil {
+			fmt.Printf("Error opening %s for output: %s\n", c.String("output"), err)
+		}
+	}
+	j := json.NewEncoder(outfile)
+	if err := j.Encode(idata); err != nil {
+		fmt.Printf("Error writing JSON to output: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	commands = append(commands, cli.Command{
+		Name:   "inspect",
+		Usage:  "inspect an image",
+		Action: inspectImage,
+	})
+}
